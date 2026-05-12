295cbb0c8c5de19f61af53bc089e4561f5b3a06f
Sarbojit Dutta <145312167+sarbojitdutta@users.noreply.github.com>
2026-05-12 09:36:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.17%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -6,17 +6,19 @@ public:
         int k = m + n - 1;
 
         while(i >= 0 && j >= 0){
-            if(nums1[i] > nums2[j]){
-                nums1[k--] = nums1[i--];
+            if(nums1[i] < nums2[j]){
+                nums1[k]=nums2[j];
+                j--;
             }else{
-                nums1[k--] = nums2[j--];
+                nums1[k]=nums1[i];
+                i--;
             }
+            k--;
         }
-        while(j >= 0){
-            nums1[k--] = nums2[j--];
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
         }
-
-        
-        
     }
 };
